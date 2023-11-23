45d57d8e78f3843d58ba35b3a9aad3e8b3d95744
getviewerspro <46578299+getviewerspro@users.noreply.github.com>
2023-11-24 01:35:05 +0300

    Update CompletePurchaseResponse.php

--- src/Message/CompletePurchaseResponse.php
+++ src/Message/CompletePurchaseResponse.php
@@ -78,8 +78,6 @@ class CompletePurchaseResponse extends AbstractResponse
         $signStr = json_encode($data, JSON_UNESCAPED_UNICODE);
     
         $sign = md5(base64_encode($signStr) . $this->request->getSecretKey());
-
-        info(['calculateSignature', $data, $sign]);
         
         return $sign;
     }
